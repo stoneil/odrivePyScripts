8e03da2850326af9019ecb3914428c226b74c734
Ian Gelman <isgelman@wpi.edu>
2018-10-25 19:34:15 -0400

    crushed the dust mites. time to submit to the forum...

--- setupHarmonicDrive.py
+++ setupHarmonicDrive.py
@@ -12,7 +12,7 @@ print("finding an odrive...")
 my_drive = odrive.find_any()
 
 print("Bus voltage is " + str(my_drive.vbus_voltage) + "V")
-print("Calibration current is " + my_drive.axis0.controller.config.calibration_current + "A")
+print("Calibration current is " + str(my_drive.axis0.motor.config.calibration_current) + "A")
 
 # Set some hardware parameters temporarily
 print("setting some hardware parameters temporarily")
@@ -23,8 +23,8 @@ my_drive.axis0.encoder.config.cpr = 400000
 
 # Calibrate
 print("starting calibration")
-my_drive.axis0.encoder.use_index = True
-my_drive.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+my_drive.axis0.encoder.config.use_index = True
+my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 
 print("waiting for calibration to end...")
 while my_drive.axis0.current_state != AXIS_STATE_IDLE:
@@ -34,14 +34,15 @@ while my_drive.axis0.current_state != AXIS_STATE_IDLE:
 print("Changing state to closed loop control")
 my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
-curr_pos = mydrive.axis0.encoder.pos_estimate
-print("current position is " + curr_pos + "ticks")
+while my_drive.axis0.current_state != AXIS_STATE_CLOSED_LOOP_CONTROL:
+    time.sleep(0.1)
+
+print("current state is " + str(my_drive.axis0.current_state))
+
+curr_pos = my_drive.axis0.encoder.pos_estimate
+print("current position is " + str(curr_pos) + " ticks")
 
 new_pos = curr_pos + 400000
-print("sending to " + new_pos + "ticks") 
+print("sending to " + str(new_pos) + " ticks") 
 my_drive.axis0.controller.pos_setpoint = new_pos
-
-
-
-
 
