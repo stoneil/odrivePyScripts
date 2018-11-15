dfbcce201286b6f4552c3fa7a53ebfaa08ca030d
Ian Gelman <isgelman@wpi.edu>
2018-11-15 15:32:02 -0500

    fixed interpreter errors. added debug stmts.

--- setupHarmonicDrive.py
+++ setupHarmonicDrive.py
@@ -12,43 +12,47 @@ print("finding an odrive...")
 my_drive = odrive.find_any()
 
 print("Bus voltage is " + str(my_drive.vbus_voltage) + "V")
-print("Calibration current is " + str(my_drive.axis0.motor.config.calibration_current) + "A")
+print("Calibration current is " + str(my_drive.axis1.motor.config.calibration_current) + "A")
 
 # Set some hardware parameters temporarily
 print("setting some hardware parameters temporarily")
-my_drive.config.brake_resistance = 0.5
-my_drive.axis0.motor.config.pole_pairs = 4
+my_drive.config.brake_resistance = 0.5 #Ohms
+my_drive.axis1.motor.config.pole_pairs = 4 #We read the datasheet
+my_drive.axis1.motor.config.resistance_calib_max_voltage = 5 #V
 
-my_drive.axis0.encoder.config.cpr = 400000
+my_drive.axis1.encoder.config.cpr = 1000
 
 # Calibrate
 print("starting calibration")
-my_drive.axis0.encoder.config.use_index = True
-my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+my_drive.axis1.encoder.config.use_index = True
+my_drive.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 
 print("waiting for calibration to end...")
-while my_drive.axis0.current_state != AXIS_STATE_IDLE:
+while my_drive.axis1.current_state != AXIS_STATE_IDLE:
     time.sleep(0.1)
+
+print("Motor phase resistance is: " + str(my_drive.axis1.motor.config.phase_resistance) + " ohms.")
 
 # Closed loop control 
 print("Changing state to closed loop control")
-my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+my_drive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
-while my_drive.axis0.current_state != AXIS_STATE_CLOSED_LOOP_CONTROL:
+while my_drive.axis1.current_state != AXIS_STATE_CLOSED_LOOP_CONTROL:
     print("axis errors are:")
-    print(hex(my_drive.axis0.error))	
+    print(hex(my_drive.axis1.error))	
     print("motor errors are:")
-    print(hex(my_drive.axis0.motor.error))
+    print(hex(my_drive.axis1.motor.error))
     print("encoder errors are:")
-    print(hex(my_drive.axis0.encoder.error))
+    print(hex(my_drive.axis1.encoder.error))
+    quit()
     time.sleep(0.1)
 
-print("current state is " + str(my_drive.axis0.current_state))
+print("current state is " + str(my_drive.axis1.current_state))
 
-curr_pos = my_drive.axis0.encoder.pos_estimate
+curr_pos = my_drive.axis1.encoder.pos_estimate
 print("current position is " + str(curr_pos) + " ticks")
 
 new_pos = curr_pos + 400000
 print("sending to " + str(new_pos) + " ticks") 
-my_drive.axis0.controller.pos_setpoint = new_pos
+my_drive.axis1.controller.pos_setpoint = new_pos
 
