132ebb8a1faa752e7473e27788cd9063f9fe56a5
stoneil <seanogon@gmail.com>
2018-11-15 16:04:15 -0500

    modified CPR and some parameters to make it move smoothly one rotation

--- setupHarmonicDrive.py
+++ setupHarmonicDrive.py
@@ -16,10 +16,18 @@ print("Calibration current is " + str(my_drive.axis0.motor.config.calibration_cu
 
 # Set some hardware parameters temporarily
 print("setting some hardware parameters temporarily")
-my_drive.config.brake_resistance = 0.5
+my_drive.config.brake_resistance = 0.3
 my_drive.axis0.motor.config.pole_pairs = 4
+my_drive.axis0.motor.config.resistance_calib_max_voltage = 5.0
+print("phase resistance: ", my_drive.axis0.motor.config.phase_resistance)
 
-my_drive.axis0.encoder.config.cpr = 400000
+my_drive.axis0.encoder.config.cpr = 4000
+
+print("position gain:",my_drive.axis0.controller.config.pos_gain, " (counts/s)/counts")
+print("vel integrator gain:",my_drive.axis0.controller.config.vel_integrator_gain, " A/((counts/s) * s)")
+print("velocity gain gain:",my_drive.axis0.controller.config.vel_gain, " A/(counts/sec)")
+my_drive.axis0.controller.config.vel_integrator_gain = 0
+my_drive.axis0.controller.config.vel_gain = 0.0001
 
 # Calibrate
 print("starting calibration")
@@ -29,6 +37,9 @@ my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 print("waiting for calibration to end...")
 while my_drive.axis0.current_state != AXIS_STATE_IDLE:
     time.sleep(0.1)
+
+
+my_drive.save_configuration()
 
 # Closed loop control 
 print("Changing state to closed loop control")
@@ -43,6 +54,7 @@ while my_drive.axis0.current_state != AXIS_STATE_CLOSED_LOOP_CONTROL:
     print(hex(my_drive.axis0.encoder.error))
     time.sleep(0.1)
 
+
 print("current state is " + str(my_drive.axis0.current_state))
 
 curr_pos = my_drive.axis0.encoder.pos_estimate
@@ -51,4 +63,7 @@ print("current position is " + str(curr_pos) + " ticks")
 new_pos = curr_pos + 400000
 print("sending to " + str(new_pos) + " ticks") 
 my_drive.axis0.controller.pos_setpoint = new_pos
+
+while(1):
+	print("position: ", my_drive.axis0.encoder.pos_estimate)
 
